3c08d118c52c8eac1f2796d0de4c59ed179e335b
chanmi1213 <166026933+chanmi1213@users.noreply.github.com>
2024-04-05 01:15:12 +0900

    update .gitignore

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712245857',resources: [
+    cacheName: 'chirpy-1712246688',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
